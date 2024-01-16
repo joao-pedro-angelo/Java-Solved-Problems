d766b86a10c022c5cc05b81d0bbf3c27ff191419
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-01-16 10:00:25 -0300

    reset PascalTriangle

--- leetcode/dynamicProgram/easy/PascalTriangle.java
+++ leetcode/dynamicProgram/easy/PascalTriangle.java
@@ -4,20 +4,5 @@ import java.util.Arrays;
 
 // https://leetcode.com/problems/pascals-triangle/description/
 public class PascalTriangle {
-    public static void main(String[] args){
-        int numRows = 5;
-        System.out.println(Arrays.toString(pascalTriangle(numRows)));
-    }
 
-    private static int[][] pascalTriangle(int numRows){
-        int[][] result = new int[1][numRows];
-        for (int i = 1; i <= numRows; i++){
-            result[i - 1] = calculatePascalTriangle(i);
-        } return result;
-    }
-
-    private static int[] calculatePascalTriangle(int numRow){
-        int[] row = new int[numRow];
-        return new int[]{1,1};
-    }
 }
