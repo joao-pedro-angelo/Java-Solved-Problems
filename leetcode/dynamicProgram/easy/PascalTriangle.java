1c11be4fb98c143af75eac6f5394bfe6eed27d70
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-01-16 10:33:32 -0300

    pascalTriangle

--- leetcode/dynamicProgram/easy/PascalTriangle.java
+++ leetcode/dynamicProgram/easy/PascalTriangle.java
@@ -1,8 +1,31 @@
 package dynamicProgram.easy;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 // https://leetcode.com/problems/pascals-triangle/description/
 public class PascalTriangle {
+    public static void main(String[] args){
+        int numRows = 5;
+        System.out.println(generate(numRows));
+    }
 
+    private static List<List<Integer>> generate(int numRows){
+        List<List<Integer>> listPascal = new ArrayList<>();
+        for (int i = 0; i < numRows; i++){
+            int[] row = new int[i + 1];
+            row[0] = 1;
+            row[row.length - 1] = 1;
+
+            for (int j = 1; j < row.length - 1; j++){
+                row[j] = listPascal.get(i - 1).get(j - 1) + listPascal.get(i - 1).get(j);
+            }
+
+            List<Integer> rowList = new ArrayList<>();
+            for (int number : row){
+                rowList.add(number);
+            } listPascal.add(rowList);
+
+        } return listPascal;
+    }
 }
