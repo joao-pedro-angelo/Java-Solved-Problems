a6e9c9747095dd84cba04050e44cf009d6a7b456
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-04-01 21:03:41 -0300

    inicia removeDuplicates

---
+++ googleChallenge/easy/removeDuplicatesSortedArray.java
@@ -0,0 +1,19 @@
+package easy;
+
+import java.util.Arrays;
+
+//leetcode.com/problems/remove-duplicates-from-sorted-array/description
+public class removeDuplicatesSortedArray {
+
+    public static void main(String[] args){
+        int[] arr = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(arr));
+        System.out.println("---");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int removeDuplicates(int[] nums){
+        return 0;
+    }
+
+}
---
+++ googleChallenge/medium/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args){
+
+    }
+
+    public static int removeDuplicates(int[] arr){
+        return 0;
+    }
+
+}
