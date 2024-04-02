b0f6f60928687287ce865fc77105e501338c7f52
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-04-02 09:21:09 -0300

    remove duplicates sorted array

--- googleChallenge/easy/removeDuplicatesSortedArray.java
+++ googleChallenge/easy/removeDuplicatesSortedArray.java
@@ -1,19 +1,36 @@
 package easy;
 
-import java.util.Arrays;
-
-//leetcode.com/problems/remove-duplicates-from-sorted-array/description
 public class removeDuplicatesSortedArray {
 
     public static void main(String[] args){
-        int[] arr = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        System.out.println(removeDuplicates(arr));
-        System.out.println("---");
-        System.out.println(Arrays.toString(arr));
+        int[] arr = new int[] {0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5};
+        int n = removeDuplicates(arr);
+        for (int i = 0; i <= n; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 
     public static int removeDuplicates(int[] nums){
-        return 0;
+        int k = 0;
+        int actual = 1;
+        int next = actual + 1;
+
+        while (actual < nums.length && next < nums.length){
+            if (nums[actual] <= nums[actual - 1]){
+                nums[actual] = nums[next];
+                next++;
+            } else{
+                actual++;
+                next = actual + 1;
+                k++;
+            }
+        }
+
+        if (nums[nums.length - 1] > nums[k]){
+            nums[++k] = nums[nums.length - 1];
+        }
+
+        return k;
     }
 
 }
